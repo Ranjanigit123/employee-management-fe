c706c5412546801162689fd4f984b49f3f8edd0d
Ranjanigit123 <ranjanirithu206@gmail.com>
2024-11-09 15:40:09 +0530

    Initial commit

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -6,7 +6,7 @@ import { Employee } from './employee';
   providedIn: 'root'
 })
 export class EmployeeService {
-  private url = 'http://localhost:5200';
+  private url = 'https://employee-management-be-1.onrender.com/employees';
   employees$ = signal<Employee[]>([]);
   employee$ = signal<Employee>({} as Employee);
   
