3420c3d9cf424c7e679151c703296cdf0adcc561
Ranjanigit123 <ranjanirithu206@gmail.com>
2024-11-09 13:36:32 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+
+//import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterOutlet } from '@angular/router'; // Ensure this is used if routing is involved
+import { MatToolbarModule } from '@angular/material/toolbar'; // Correct import
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone: true,
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule], // Ensure these modules/components are used
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], 
+  styles: [
+    `
+      main {
+        display: flex;
+        justify-content: center;
+        padding: 2rem 4rem;
+      }
+    `,
+  ],
+  template: `
+    <mat-toolbar>
+      <span>Employees Management System</span>
+    </mat-toolbar>
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+  `,
 })
 export class AppComponent {
   title = 'client';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+//app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,14 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  //declarations: [
+   // AppComponent
+  //],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  //bootstrap: [AppComponent]
 })
 export class AppModule { }
